29b04e00f380bec90aaf7e1a47447bf735f647b4
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2024-10-07 21:32:23 +0300

    written in class format

--- age_calculate.py
+++ age_calculate.py
@@ -1,15 +1,23 @@
 from datetime import datetime
+from tkinter import * 
+from tkinter import messagebox
+class calculator:
+    def __init__(self,name):
+        self.name = name
+        self.today = datetime.now()
+        self.loop = True
+    def calculate(self):
+        while self.loop:
+            try:
+                birthdate = input("Enter your birthdate (YY-MM-DD) : ")
+                formated_birthdate = datetime.strptime(birthdate,'%Y-%m-%d')
+                age = self.today.year - formated_birthdate.year
+                print(f"{name}, you are {age} years old")
+                self.loop = False
+            except ValueError:
+                print("Invalid format. Please enter as YYYY-MM-DD ")
 
-name = str(input("Enter your name : "))
-today = datetime.now()
-loop = True
-
-while loop :
-    try:
-        birthdate = input("Enter your birthdate (YY-MM-DD) : ")
-        formated_birthdate = datetime.strptime(birthdate,'%Y-%m-%d')
-        age = today.year - formated_birthdate.year
-        print(f"{name}, you are {age} years old")
-        loop = False
-    except ValueError:
-        print("Invalid format. Please enter as YYYY-MM-DD ")
+if __name__ == "__main__":
+    name = input("Enter your name : ")
+    calc = calculator(name)
+    calc.calculate()
