72cabe2366923e30804a5d9be8ef20438ca4b6c7
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2024-10-10 12:50:03 +0300

    added ui with tkinter

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ This Python script calculates a user's age based on their date of birth.
 # Usage
 just run " python age_calculate.py " in terminal
 # Showcase
-![alt text](image.png)
+![alt text](image-1.png)
 ## Author
 Furkan Sarsilmaz
--- age_calculate.py
+++ age_calculate.py
@@ -1,23 +1,39 @@
+from tkinter import *
 from datetime import datetime
-from tkinter import * 
+import re
 from tkinter import messagebox
-class calculator:
-    def __init__(self,name):
-        self.name = name
-        self.today = datetime.now()
-        self.loop = True
-    def calculate(self):
-        while self.loop:
-            try:
-                birthdate = input("Enter your birthdate (YY-MM-DD) : ")
-                formated_birthdate = datetime.strptime(birthdate,'%Y-%m-%d')
-                age = self.today.year - formated_birthdate.year
-                print(f"{name}, you are {age} years old")
-                self.loop = False
-            except ValueError:
-                print("Invalid format. Please enter as YYYY-MM-DD ")
 
+class calculator():
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("300x200")
+        self.root.title("Age Calculator")
+        self.root.configure(background="gray")
+        self.name_text = Text(self.root,width=15,height=1)
+        self.name_text.pack(pady=5)
+
+        self.birthdate_text = Text(self.root,width=15,height=1)
+        self.birthdate_text.pack(pady=5)
+
+        self.calc_button = Button(self.root,text="Calculate",width=7,height=2,command=self.calculate)
+        self.calc_button.pack()
+
+        self.today = datetime.now()
+
+    def calculate(self):
+        name = self.name_text.get("1.0",END).strip()
+        birthdate = self.birthdate_text.get("1.0",END).strip()
+        try:
+            formated_birthdate = datetime.strptime(birthdate,'%Y-%m-%d')
+            age = self.today.year - formated_birthdate.year
+            messagebox.showinfo("Succeed",f"{name}, you are {age} old.")
+        except ValueError:
+            messagebox.showerror("Error","Use YYYY-MM-DD format ")
+            self.name_text.delete("1.0",END)
+            self.birthdate_text.delete("1.0",END)
+            return
+        
 if __name__ == "__main__":
-    name = input("Enter your name : ")
-    calc = calculator(name)
-    calc.calculate()
+    root = Tk()
+    calculator(root)
+    root.mainloop()
